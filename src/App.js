8c911c532c1f39bdbc81728e8ffcc825e176b49f
radex171 <radex171@gmail.com>
2023-01-07 12:32:22 +0100

    feature: Add Context

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Views/Home/Home";
-function App() {
-  return (
-    <div className="w-screen flex flex-wrap m-0">
-      <header className="w-screen h-auto">
-        <Header />
-      </header>
+import { useState } from "react";
+import Context from "./Context/Context";
 
-      <section className="w-10/12 mx-auto my-2 flex justify-center">
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/AddReport" element={""} />
-          <Route path="/Events" element={""} />
-        </Routes>
-      </section>
-    </div>
+function App() {
+  const [wallet, setWallet] = useState(null);
+  return (
+    <Context.Provider value={{ wallet, setWallet }}>
+      <div className="w-screen flex flex-wrap m-0">
+        <header className="w-screen h-auto">
+          <Header />
+        </header>
+
+        <section className="w-10/12 mx-auto my-2 flex justify-center">
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/AddReport" element={""} />
+            <Route path="/Events" element={""} />
+          </Routes>
+        </section>
+      </div>
+    </Context.Provider>
   );
 }
 
